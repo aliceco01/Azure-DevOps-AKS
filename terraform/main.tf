89834f7bc4ff53f1928ef24cc096b65bf2675d80
Alice C <83873276+aliceco01@users.noreply.github.com>
2025-03-09 18:05:05 +0200

    removed backend configuration from main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "myResourceGroup"
-    storage_account_name  = "mytfstateaccount"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
 
 provider "azurerm" {
   features {}
