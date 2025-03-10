93f678e8ce3d0296e38945e02bb62c117fd6bfff
Alice C <83873276+aliceco01@users.noreply.github.com>
2025-03-10 03:09:27 +0200

    added acr module outputs

--- terraform/modules/acr/main.tf
+++ terraform/modules/acr/main.tf
@@ -1,15 +1,18 @@
-
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = var.sku
-  
-  admin_enabled = true
- 
-  tags = var.tags
+  admin_enabled       = false  # Disable admin user for security; use managed identity instead
+  tags                = var.tags
 }
 
 output "login_server" {
-  value = azurerm_container_registry.acr.login_server
+  description = "The login server URL of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.login_server
+}
+
+output "acr_id" {
+  description = "The ID of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.id
 }
---
+++ terraform/modules/acr/outputs.tf
@@ -0,0 +1,21 @@
+output "acr_id" {
+  description = "The ID of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.id
+}
+
+output "login_server" {
+  description = "The login server URL of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.login_server
+}
+
+output "admin_username" {
+  description = "The admin username for the Azure Container Registry (if admin_enabled = true)"
+  value       = azurerm_container_registry.acr.admin_username
+  sensitive   = true
+}
+
+output "admin_password" {
+  description = "The admin password for the Azure Container Registry (if admin_enabled = true)"
+  value       = azurerm_container_registry.acr.admin_password
+  sensitive   = true
+}
